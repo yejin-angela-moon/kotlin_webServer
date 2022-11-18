22632e65ab2c2e611ce2e33eef68a23629f3d3d6
ym1721 <ym1721@ic.ac.uk>
2022-11-18 17:40:09 +0000

    st check

--- src/main/kotlin/webserver/Http.kt
+++ src/main/kotlin/webserver/Http.kt
@@ -7,65 +7,65 @@ class Request(val url: String, val authToken: String = "")
 class Response(val status: Status, val body: String = "")
 
 enum class Status(val code: Int) {
-  OK(200),
-  FORBIDDEN(403),
-  NOT_FOUND(404)
+    OK(200),
+    FORBIDDEN(403),
+    NOT_FOUND(404)
 }
 
 fun helloHandler(request: Request): Response {
-  val name =
-    when {
-      request.url.contains("name=") ->
+    val name =
         when {
-          request.url.contains("style=") -> request.url.substringAfter("name=").substringBefore("&")
-          else -> request.url.substringAfter("name=")
-        }
+            request.url.contains("name=") ->
+                when {
+                    request.url.contains("style=") -> request.url.substringAfter("name=").substringBefore("&")
+                    else -> request.url.substringAfter("name=")
+                }
 
-      else -> "World"
+            else -> "World"
+        }
+    val body = "Hello, $name!"
+    if (request.url.substringAfter("style=") == "shouting") {
+        return Response(Status.OK, body.uppercase())
     }
-  val body = "Hello, $name!"
-  if (request.url.substringAfter("style=") == "shouting") {
-    return Response(Status.OK, body.uppercase())
-  }
-  return Response(Status.OK, body)
+    return Response(Status.OK, body)
 }
 
 fun homepageHandler(request: Request): Response {
-  val pages = mapOf("/" to "Imperial", "/computing" to "DoC")
-  val host = request.url.substringAfter("://").substringBefore("/")
-  val path = when {
-    request.url.contains("?") -> request.url.substringAfter(
-      request.url.substringAfter("://")
-        .substringBefore("/")
-    ).substringBefore("?")
+    val pages = mapOf("/" to "Imperial", "/computing" to "DoC")
+    val host = request.url.substringAfter("://").substringBefore("/")
+    val path = when {
+        request.url.contains("?") -> request.url.substringAfter(
+            request.url.substringAfter("://")
+                .substringBefore("/")
+        ).substringBefore("?")
 
-    else -> request.url.substringAfter(request.url.substringAfter("://").substringBefore("/"))
-  }
-  val value = pages.getValue(path)
-  if (pages.containsKey(path)) {
-    return Response(Status.OK, "This is $value.")
-  }
-  return Response(Status.NOT_FOUND, "")
+        else -> request.url.substringAfter(request.url.substringAfter("://").substringBefore("/"))
+    }
+    val value = pages.getValue(path)
+    if (pages.containsKey(path)) {
+        return Response(Status.OK, "This is $value.")
+    }
+    return Response(Status.NOT_FOUND, "")
 }
 
 fun route(request: Request): Response {
-  val path =
-    when {
-      request.url.contains("?") -> request.url.substringAfter(
-        request.url.substringAfter("://")
-          .substringBefore("/")
-      ).substringBefore("?")
+    val path =
+        when {
+            request.url.contains("?") -> request.url.substringAfter(
+                request.url.substringAfter("://")
+                    .substringBefore("/")
+            ).substringBefore("?")
 
-      else -> request.url.substringAfter(request.url.substringAfter("://").substringBefore("/"))
+            else -> request.url.substringAfter(request.url.substringAfter("://").substringBefore("/"))
+        }
+    if (path == "/" || path == "/computing") {
+        return homepageHandler(request)
+        //} else if (path.contains("/search")) {
+        //return searchPageHandler(request)
+    } else if (path.contains("say-hello")) {
+        return helloHandler(request)
     }
-  if (path == "/" || path == "/computing") {
-    return homepageHandler(request)
-    //} else if (path.contains("/search")) {
-    //return searchPageHandler(request)
-  } else if (path.contains("say-hello")) {
-    return helloHandler(request)
-  }
-  return Response(Status.NOT_FOUND, "")
+    return Response(Status.NOT_FOUND, "")
 }
 
 //typealias HttpHandler = (Request) -> Response
