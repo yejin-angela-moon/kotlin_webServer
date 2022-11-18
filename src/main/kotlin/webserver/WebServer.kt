a739a9b482d85ae62d801556fc8cfdb352ccd20b
ym1721 <ym1721@ic.ac.uk>
2022-11-18 17:54:16 +0000

    indent

--- src/main/kotlin/webserver/WebServer.kt
+++ src/main/kotlin/webserver/WebServer.kt
@@ -8,17 +8,17 @@ fun scheme(url: String): String = url.substringBefore("://")
 fun host(url: String): String = url.substringAfter("://").substringBefore("/")
 
 fun path(url: String): String =
-    when {
-        url.contains("?") -> url.substringAfter(host(url)).substringBefore("?")
-        else -> url.substringAfter(host(url))
-    }
+  when {
+    url.contains("?") -> url.substringAfter(host(url)).substringBefore("?")
+    else -> url.substringAfter(host(url))
+  }
 
 fun queryParams(url: String): List<Pair<String, String>> =
-    when {
-        url.substringAfter(host(url)) == "/" -> emptyList()
-        else ->
-            url.substringAfter("?").split("&").map { x -> Pair(x.substringBefore("="), x.substringAfter("=")) }
-    }
+  when {
+    url.substringAfter(host(url)) == "/" -> emptyList()
+    else ->
+      url.substringAfter("?").split("&").map { x -> Pair(x.substringBefore("="), x.substringAfter("=")) }
+  }
 
 
 // http handlers for a particular website...
